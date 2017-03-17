ff159223207c4c98dd80c1dffe0997786c4575db
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-03-17 20:10:39 +0100

    add schemas of db

---
+++ models/usuarioBarter.js
@@ -0,0 +1,12 @@
+//Esquema de como vamos a guardar a los usuarios
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+//El usuario por defecto de barter
+var usuario = Schema({
+
+
+});
