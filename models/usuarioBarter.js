57efa91f2642cbabf1a2875ece36078efdffe6bf
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-03-17 23:31:51 +0100

    update schemas db

--- models/usuarioBarter.js
+++ models/usuarioBarter.js
@@ -6,7 +6,15 @@ var Schema = mongoose.Schema;
 
 
 //El usuario por defecto de barter
-var usuario = Schema({
-
-
+var usuarioSchema = Schema({
+    name: String,
+    surname: String,
+    companyname: String
+    //id: Integer, 
+    //data: Data
 });
+
+
+//Este esquema actuara como modelo , que representara al esquema creado mas arriba
+//Osea si hacemos un nuevo usuario sera con todas las caracteristicas de arriba 
+module.export = mongoose.model('Usuario', usuarioSchema);
