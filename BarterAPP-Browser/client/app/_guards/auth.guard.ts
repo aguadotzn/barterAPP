a8ddd44f68cf8ad7141bb7c914ba339d3fe4f85b
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-06-08 15:47:33 +0200

    reconstruct #client

---
+++ BarterAPP-Browser/client/app/_guards/auth.guard.ts
@@ -0,0 +1,19 @@
+//Guards: controla el acceso a las diferentes rutas
+﻿import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (localStorage.getItem('currentUser')) {
+            // Usuario logeado, devuelve true
+            return true;
+        }
+        // Usuario no logeado, le devuelve a la pagina de login de nuevo
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        return false;
+    }
+}
