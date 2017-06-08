dbce78b5a687656b7738f7f6fcea28b3bf0a2b51
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-06-08 19:24:06 +0200

    reconstruct #server

--- BarterAPP-Browser/server/services/event.service.js
+++ BarterAPP-Browser/server/services/event.service.js
@@ -1,8 +1,7 @@
-/*Servicios: controla los eventos*/
-
+/*Servicios: controla los turnos */
 var config = require('config.json')
-// var _ = require('lodash')
-// var jwt = require('jsonwebtoken')
+var _ = require('lodash')
+var jwt = require('jsonwebtoken')
 var bcrypt = require('bcryptjs')
 var Q = require('q')
 var mongo = require('mongoskin')
@@ -13,20 +12,17 @@ db.bind('users')
 
 var service = {}
 
-//Monkoskins test
+// Monkoskins test, para comprobar si funciona
 service.testMongoskin = function (param) {
-	var deferred = Q.defer()
+  var deferred = Q.defer()
 	 console.log('params: +' + JSON.stringify(param))
 	 var company = param.company
 	 var event = param.event
-	 console.log("_______________________________");
 	 var query = { company: company, 'username': {"$ne": event.username},'type': "Free Shifts",'status':'normal' }
-	 //console.log("testMOngoskin HOLA: " + JSON.stringify(query))
+	 //console.log("testMongoskin HOLA: " + JSON.stringify(query))
 	 db.event.find(query).toArray(function (err, events){
 		if ( err ) deffered.reject(err.name + ': ' + err.message)
-		console.log("result : " + JSON.stringify(events))
 		if(events.length > 0){
-
 			deferred.resolve(events)
 		}else
 			var emptyObject = {}
@@ -44,3 +40,163 @@ service.getAll = function(){
     })
  return deferred.promise
 }
+
+// Un usuario especifico de una determinada empresa
+service.getSpecialByCompanyByWorker = function(param){
+  var deferred = Q.defer()
+	var query = {'company' : param.company, 'username': param.username, 'status':{"$ne": param.status} };
+	db.event.find(query).toArray(function (err, events) {
+	// En caso de error, se muestra
+  if (err) deferred.reject(err.name + ': ' + err.message)
+  deferred.resolve(events)
+})
+  return deferred.promise
+}
+
+// Obtener todos por empresa
+service.getAllByCompany = function (param) {
+  var deferred = Q.defer()
+  db.event.find({'company': param.company}).toArray(function (err, events) {
+  // En caso de error, se muestra
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    deferred.resolve(events)
+  })
+  return deferred.promise
+}
+
+//Obtener todos los usuarios de una determinada empresa
+service.getAllByCompanyByWorker = function (param) {
+// console.log("param in get all object = : " + JSON.stringify(param));
+  var deferred = Q.defer()
+  db.event.find({'company': param.company, 'username': param.username}).toArray(function (err, event) {
+  // En caso de error, se muestra
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    deferred.resolve(event)
+  })
+  return deferred.promise
+}
+
+// Obtener usuarios "libres"
+service.getFreeUsers = function (param) {
+  var deferred = Q.defer()
+  var date = new Date(param.searchDay)
+  var firstDay = new Date(date.getFullYear(), date.getMonth(), 1)
+  var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+  db.event.find({ 'company': param.company,
+	'username': {"$ne": param.username},
+	'type': "Free Shifts",
+	'start': { "$gte": firstDay.toISOString()},
+	'end': { "$lte": lastDay.toISOString() } }).toArray(function (err, event) {
+  if (err) deferred.reject(err.name + ': ' + err.message)
+  if (event.length > 0) {
+      var userString = ''
+    for (var i in event) {
+        var name = event[i].username
+        var start = event[i].start.substr(0,10)
+        var end = event[i].end.substr(0,10)
+        userString += 'Usuario'+name + '" ' + start + '~'+ end + "\n";
+    }
+  }
+  else
+      userString = 'none'
+  console.log(userString)
+  var dev = {'users': userString}
+  deferred.resolve(dev)
+  })
+  return deferred.promise
+}
+
+// Obtener usuarios "libres" por empresa y dia
+service.findFreeUsersByCompanyByDayByShift = function(param) {
+	  var deferred = Q.defer();
+	  //console.log("Soy un evento con parametro/dia = " + param.searchDay);
+	  //console.log("Soy un evento con parametro/usuario = " + param.userName);
+	  //console.log("Shift : " + param.shift);
+
+	  var userQuery = {cname: param.company, shift: param.shift, 'username': {"$ne": param.userName}};
+	  console.log("use query object = : " +JSON.stringify(userQuery));
+
+	  db.users.find(userQuery).toArray(function(err, sameShiftUsers){
+		  if (err) deferred.reject(err.name + ': ' + err.message)
+
+		  if (sameShiftUsers.length > 0) {
+			  var usernameList = [];
+			  //console.log("Mismo cambio usuarios: + " + JSON.stringify(sameShiftUsers))
+			    for(var i = 0; i < sameShiftUsers.length; i++){
+			    	usernameList.push(sameShiftUsers[i].username)
+			    }
+
+
+		  	  var query = { company: param.company , 'username': { $in: usernameList},'type': "Free Shifts",'status':'normal','start': { "$eq": param.searchDay} };
+
+			  db.event.find(query).toArray(function (err, events) {
+			    if (err) deferred.reject(err.name + ': ' + err.message);
+			    if (events.length > 0) {
+			    	//console.log("Eventos: + " + JSON.stringify(events))
+			     deferred.resolve(events);
+			    }
+			    else{
+			    	var emptyObject = {}
+					deferred.resolve(emptyObject)
+			    }
+			  })
+
+      }else{
+		    	var emptyObject = {}
+				deferred.resolve(emptyObject)
+		    }
+	  })
+return deferred.promise
+	}
+
+//Crear servicios
+service.create = function(param) {
+    var deferred = Q.defer();
+        db.event.insert( param, function (err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            var insertedEvent = doc.ops;
+            insertedEvent._id = doc.insertedIds;
+            //     console.log("Evento Insertador" + JSON.stringify(insertedEvent[0]));
+            // 	   mongoskin insert return an array that is why you must return only first item from array.
+            deferred.resolve(insertedEvent[0]);
+        })
+return deferred.promise
+}
+
+//Actualizar un servicio
+service.update = function(param) {
+    var deferred = Q.defer()
+    var set = {
+      title: param.title,
+      start: param.start,
+      end: param.end,
+      primary_color: param.primary_color,
+      secondary_color: param.secondary_color,
+      type: param.type,
+      status: param.status
+    };
+    console.log("Servicio actualizado!!!!");
+    //console.log("establecido? : " +JSON.stringify(set));
+    db.event.updateById(param._id,
+        { $set: set },
+        function (err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            var emptyObject = {};
+			deferred.resolve(emptyObject);
+        });
+
+    return deferred.promise;
+}
+
+service._delete = function (param) {
+  var deferred = Q.defer()
+  // console.log("El siguiente servicio se ha borrado: " + param.eventId);
+  db.event.removeById(param.eventId, function (err) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    deferred.resolve()
+  })
+  return deferred.promise
+}
+
+module.exports = service
---
+++ BarterAPP-Browser/server/services/user.service.js
@@ -0,0 +1,167 @@
+/* Servicios: controla los usuarios */
+var config = require('config.json')
+var _ = require('lodash')
+var jwt = require('jsonwebtoken')
+var bcrypt = require('bcryptjs')
+var Q = require('q')
+var mongo = require('mongoskin')
+var db = mongo.db(config.connectionString, {
+})
+db.bind('users')
+var service = {}
+
+service.authenticate = authenticate
+service.getAll = getAll
+service.getById = getById
+service.create = create
+service.update = update
+service.delete = _delete
+
+module.exports = service
+
+// Autenticacion
+function authenticate (email, password) {
+  var deferred = Q.defer()
+  db.users.findOne({ email: email }, function (err, user) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+
+    if (user && bcrypt.compareSync(password, user.hash)) {
+      // Si la autenticacion del usuario es correcta
+      deferred.resolve({
+        _id: user._id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        cname: user.cname,
+        shift: user.shift,
+        username: user.username,
+        token: jwt.sign({ sub: user._id }, config.secret)
+      })
+    } else { // Si la autentiacion falla
+      deferred.resolve()
+    }
+  })
+  return deferred.promise
+}
+
+// Obtener todos
+function getAll () {
+  var deferred = Q.defer()
+  db.users.find().toArray(function (err, users) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    // devuelve los usuarios (sin contraseñas)
+    users = _.map(users, function (user) {
+      return _.omit(user, 'hash')
+    })
+    deferred.resolve(users)
+  })
+  return deferred.promise
+}
+
+// Obtener por id
+function getById (_id) {
+  var deferred = Q.defer()
+  db.users.findById(_id, function (err, user) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    if (user) {
+      // devuelve los usuarios (sin contraseñas)
+      deferred.resolve(_.omit(user, 'hash'))
+    } else {
+     // usuario no encontrado
+      deferred.resolve()
+    }
+  })
+  return deferred.promise
+}
+
+// Crear un usuario 1
+function create (userParam) {
+  var deferred = Q.defer()
+    // validacion
+  db.users.findOne(
+        { email: userParam.email },
+        function (err, user) {
+          if (err) deferred.reject(err.name + ': ' + err.message)
+          if (user) {
+                // si el nombre ya existe
+            deferred.reject('Email "' + userParam.email + '" ya en uso.')
+          } else {
+            createUser()
+          }
+        })
+  // Despues de la validacion se crea el usuario
+  function createUser () {
+        // Se estableccen los datos del usuario omitiendo el campo contraseña
+    var user = _.omit(userParam, 'password')
+    // añade la contraseña hash al usuario para almacenarla en la db
+    user.hash = bcrypt.hashSync(userParam.password, 10)
+    db.users.insert(
+            user,
+            function (err, doc) {
+              if (err) deferred.reject(err.name + ': ' + err.message)
+              deferred.resolve()
+            })
+  }
+  return deferred.promise
+}
+
+// Actualizar usuario
+// (Para version 2.0)
+function update (_id, userParam) {
+  var deferred = Q.defer()
+  // Validacion
+  db.users.findById(_id, function (err, user) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    if (user.username !== userParam.username) {
+        // El nombre de usuario puede cambiar luego comprobamos que no este en la db
+      db.users.findOne({ username: userParam.username },
+            function (err, user) {
+              if (err) deferred.reject(err.name + ': ' + err.message)
+              if (user) {
+                // Si ya eviste mostramos un mensaje
+              } else {
+                updateUser()
+              }
+            })
+    } else {
+      updateUser()
+    }
+  })
+  function updateUser () {
+        // Campos actualizar (o posibles)
+    var set = {
+      firstName: userParam.firstName,
+      email: userParam.email,
+      cname: userParam.cname,
+      lastName: userParam.lastName,
+      username: userParam.username
+    }
+
+    // Actualizar contraseña (si es necesario)
+    if (userParam.password) {
+      set.hash = bcrypt.hashSync(userParam.password, 10)
+    }
+
+    db.users.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+              if (err) deferred.reject(err.name + ': ' + err.message)
+              deferred.resolve()
+            })
+  }
+  return deferred.promise
+}
+
+// Eliminar usuario
+function _delete (_id) {
+  var deferred = Q.defer()
+
+  // Se eliminar de la base de datps
+  db.users.remove({ _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+          if (err) deferred.reject(err.name + ': ' + err.message)
+          deferred.resolve()
+        })
+  return deferred.promise
+}
