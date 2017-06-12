fa9d6df5713ab2bf483ea95a5c73e6dc4137bd17
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-06-12 17:48:36 +0200

    reconstruct #client #calendar

--- BarterAPP-Browser/client/app/pages/angular_calendar/angular_calendar.component.ts
+++ BarterAPP-Browser/client/app/pages/angular_calendar/angular_calendar.component.ts
@@ -1,20 +1,19 @@
-//Pages: Calendario
+//Pages: Calendario principal de la app
+//https://github.com/mattlewis92/angular-calendar
+//https://github.com/linkedin/oncall
 import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef, OnInit, AfterViewInit, NgZone, ApplicationRef  } from '@angular/core';
 
 /*RxJS*/
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/first';
-
 /*models*/
 import { User, ShiftEventModel, MyEvent, EventDTO, InterchangeDTO } from 'app/_models/index';
 /*services*/
 import { CalendarRepository } from 'app/repository/calendar.repository';
 /*utils*/
 import { ShiftUtilsService } from 'app/utils/shift.utils.service';
-
 /*Bootstrap-ng*/
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 /*Calendar*/
 import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours } from 'date-fns';
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent } from 'angular-calendar';
@@ -28,12 +27,9 @@ import { Message } from 'primeng/primeng';
   styleUrls: ['styles.css'],
   templateUrl: 'angular_calendar.component.html'
 })
-
 export class AngularCalendarComponent implements AfterViewInit {
 
-
   private activeDayIsOpen: boolean = true;
-
   // Bandera que se asigna en función de la selección del usuario
   showNormalEvent = 0; // 0 - normal, 1 - pending, 2 - decline, 3 - accepted
   // Contadores que muestran en los botones el numero de peticiones
@@ -41,7 +37,7 @@ export class AngularCalendarComponent implements AfterViewInit {
   declineEventCounter: number = 0; //Rechazadas
   acceptedEventCounter: number = 0; //Aceptadas
 
-  //Usuario previamente autenticado
+  //Usuario previamente autenticado (Usuario Actual)
   currentUser: User;
 
   //Variable que crea shiftEvent (con las caracteristicas en /models)
@@ -50,12 +46,17 @@ export class AngularCalendarComponent implements AfterViewInit {
  // Turnos  ["24 Horas", "12 Horas" , "8 Horas", "6 Horas"];
   shifts;
 
-  //Tipos  'Assigned shifts' , 'Desired free shifts','Untouchable free shifts','Free Shifts'
+  //Tipos  'Assigned shifts' , 'Desired free shifts', 'Untouchable free shifts', 'Free Shifts'
+  /*
+  Assigned shifts :  Asignado conforme
+  Desired free shifts : Asignado eliminar
+  Untouchable free shifts : Intocable
+  Free Shifts : Libre
+  */
   types;
 
   // Colores preasignados a cada tipo
   colors;
-
 
   constructor(private calendarRepository: CalendarRepository, shiftUtilsService: ShiftUtilsService) {
     this.colors = shiftUtilsService.getColors();
@@ -63,34 +64,23 @@ export class AngularCalendarComponent implements AfterViewInit {
     this.types = shiftUtilsService.getTypes();
   }
 
-  
+  // Vista por defecto del calendario
   public view = 'month';
 
   viewDate: Date = new Date();
 
-  /* Acciones proporcionadas por el calendario, no se usan
-  actions: CalendarEventAction[] = [{
-    label: '<i class="fa fa-fw fa-pencil"></i>',
-    onClick: ({event}: {event: MyEvent}): void => {
-      this.handleEvent('Edited', event);
-    }
-  }, {
-    label: '<i class="fa fa-fw fa-times"></i>',
-    onClick: ({event}: {event: MyEvent}): void => {
-      this.events = this.events.filter(iEvent => iEvent !== event);
-      this.handleEvent('Deleted', event);
-    }
-  }];*/
-
-
-  
+  // Calendario
   refresh: Subject<any> = new Subject();
 
   // todos los eventos que tiene el usuario actual
   events: MyEvent[] = [];
 
 
- 
+  // msgs PrimeNg growl component
+  msgs: Message[] = [];
+
+  // stickymsgs PrimeNg growl component
+  stickymsgs: Message[] = [];
 
   // free users for current date
   freeUsers: Array<MyEvent> =[];
@@ -121,7 +111,6 @@ export class AngularCalendarComponent implements AfterViewInit {
  https://angular.io/docs/ts/latest/api/core/index/AfterViewInit-class.html
  */
   ngAfterViewInit(): void {
-
     this.currentUser = JSON.parse(localStorage.currentUser);
     //Esta funcion se encuentra en el servidor
     this.calendarRepository.getEventsByCompanyByWorker(this.currentUser.cname, this.currentUser.username).subscribe(result => {
@@ -145,7 +134,9 @@ export class AngularCalendarComponent implements AfterViewInit {
 
         this.populateSpecificEventsCollections(currentEvent);
 
-     
+        /* Muestra el numero de turnos / dia (numero de circulos)
+         Hay que tener en cuenta que funcionalmente
+         en la  primera version no sirve para nada*/
         let dotsCount = this.shifts.indexOf(this.currentUser.shift)+1;
         let shiftEvent = new ShiftEventModel(currentEvent, v, dotsCount);
         this.shiftEvents.push(shiftEvent);
@@ -157,7 +148,7 @@ export class AngularCalendarComponent implements AfterViewInit {
 
     this.calendarRepository.initializePolling(this.currentUser.cname, this.currentUser.username, 'normal').subscribe(result => {
 
-      //Variables locales
+      //Variables locales para el calendario
       let pendingEvents = [];
       this.acceptedEvents  = [];
       this.declineEvents  = [];
@@ -181,9 +172,26 @@ export class AngularCalendarComponent implements AfterViewInit {
           sender: result[i].sender};
 
         let element = currentEvent;
-   
+        //Switch para controlar el tipo de PETICIONES
+        switch(element.status) {
+          case "pending": {
+            pendingEvents.push(element);
+            break;
+          }
+          case "declined": {
+            this.declineEvents.push(element);
+            break;
+          }
+          case "accepted": {
+            this.acceptedEvents.push(element);
+            break;
+          }
+          default: {
+            break;
+          }
+        }
       }
-     
+      // primeNg growl: muestra mensajes
       this.msgs = [];
 
       // Comprueba si la cantidad de eventos pendientes ha cambiado
@@ -207,11 +215,7 @@ export class AngularCalendarComponent implements AfterViewInit {
         // Cambia el número de eventos aceptados
       this.acceptedEventCounter = this.acceptedEvents.length;
 
-      //Nota: los contadores se usan en el template (ngFor*)
-
-
-
-
+      //Nota: los contadores se establecene en el template (ngFor*)
 
       let originalEvent;
       for (let i= 0; i< this.shiftEvents.length; i++ ){
@@ -219,8 +223,6 @@ export class AngularCalendarComponent implements AfterViewInit {
         let currentShiftEvent = this.shiftEvents[i];
         // Comprueba eventos pendientes
         for(let j = 0; j < pendingEvents.length; j++){
-
-
 
           //Si encuentra un evento (pendiente) en el calendario
           if(currentShiftEvent.isEqual(pendingEvents[j]._id)){
@@ -268,12 +270,38 @@ export class AngularCalendarComponent implements AfterViewInit {
     });
   }//ngAfterViewInit
 
+/*
+Función para aumentar los contadores
+*/
+  private populateSpecificEventsCollections(currentEvent) {
+    switch (currentEvent.status) {
+      case "pending":
+      {
+        this.pendingEventCounter++;
+        break;
+      }
+      case "declined":
+      {
+        this.declineEventCounter++;
+        break;
+      }
+      case "accepted":
+      {
+        this.acceptedEventCounter++;
+        break;
+      }
+      default:
+      {
+        break;
+      }
+    }
+  }
 
-
+  // PrimeNG growl: muestra mensaje
   addMessage(messageText){
     this.msgs.push(messageText);
   }
-  
+  // PrimeNG growl: muestra mensaje
   addStickyMessage(messageText){
     this.stickymsgs.push(messageText);
   }
@@ -303,7 +331,7 @@ export class AngularCalendarComponent implements AfterViewInit {
          return;
         }
         /*
-        Podemos hacer facilmente que los libre sean intercambiables, pero no tiene sentido
+        Podríamos hacer facilmente que los libre sean intercambiables, pero no tiene sentido
         if(markedEvent.type == this.types[0] || markedEvent.type == this.types[2]){
           return;
         }*/
@@ -345,13 +373,14 @@ export class AngularCalendarComponent implements AfterViewInit {
       sender: ''
     };
 
-
+    //En el calendario orignal se establece un fecha de inicio y final
+    //aqui no nos interesa, en principio, estas variables
+    //Lo que sí hay que hacer es eliminar la fecha y hora
     this.clearDateTime(params.start);
     this.clearDateTime(params.end);
 
     // Llamada http para crear el evento en la BD
     this.calendarRepository.createEvent(params).subscribe(result => {
-
       let currentEvent = <MyEvent> { _id    : result._id,
         type      : result.type,
         title     : result.title,
@@ -368,20 +397,15 @@ export class AngularCalendarComponent implements AfterViewInit {
         status: result.status,
         sender: result.sender};
 
-
       // Aqui se calcula el numero de circulos
       /* Hay que tener en cuenta que funcionalmente
        en la  primera version no sirve para nada*/
       let dotsCount = this.shifts.indexOf(this.currentUser.shift)+1;
-      console.log("dotsCount  : " +dotsCount);
-
 
       let shiftEvent =  new ShiftEventModel(currentEvent, this.events.length, dotsCount);
       this.shiftEvents.push(new ShiftEventModel(currentEvent, this.events.length, dotsCount));
 
       shiftEvent.populateOriginalEventList(this.events);
-
-
 
       // Se refresca el calendario por que se han añadido nuevos elementos
       this.refresh.next();
@@ -389,7 +413,20 @@ export class AngularCalendarComponent implements AfterViewInit {
     });
   }
 
-
+  /* Eliminar un nuevo evento */
+  deleteEvent(index): void {
+    // El elemento a eliminar se elimina por un indice establecido en el template (ngFor)
+    let shiftEvent = this.shiftEvents[index];
+    // Llamada http para eliminar en la base de datos
+    this.calendarRepository.deleteEvent(shiftEvent.getOriginalEvent()._id).subscribe(
+        result => {
+        // Se elimina de la lista original
+        shiftEvent.deleteFromOriginalEventList(this.events, index);
+        shiftEvent.deleteOneSelf(this.shiftEvents, index);
+        // Se refresca el calendario por que se han eliminado nuevos elementos
+        this.refresh.next();
+      });
+  }
 
   // Editar eventos
   valueChange(index): void {
@@ -397,7 +434,6 @@ export class AngularCalendarComponent implements AfterViewInit {
     let changedShiftEvent = this.shiftEvents[index];
     // Se cambia el tipo en funcion de las preferencias del usuario
     this.changeTypeShiftEvent(changedShiftEvent);
-
     //Cuando el usuario cambia la fecha hay que borrarla y crear una nueva
     this.clearDateTime(changedShiftEvent.getStartDate());
     // Igual que antes la fecha de inicio = a la fecha final (no nos interesa hacer distinciones)
@@ -438,8 +474,6 @@ export class AngularCalendarComponent implements AfterViewInit {
 
   /*Método peticón aceptada*/
   public accept(index) {
-
-
 
     //Cuando se acepta el intercambio debes enviar el evento para intercambiarlo con el evento del receptor
     //por lo tanto la coleccion 'interchange' debe ser usada
@@ -496,7 +530,6 @@ export class AngularCalendarComponent implements AfterViewInit {
     });
 
   }
-
   /*Método peticón rechazada*/
   public decline(index) {
     // Se escoge el elemento por el índice que éste tiene, establecido en el template (ngFor)
@@ -554,14 +587,41 @@ export class AngularCalendarComponent implements AfterViewInit {
 
     }
     return null;
-
   }
 
+  // Cambiar el estado de un evento (por id)
+  private changeEventStatusById(id, status){
+    for(let i = 0; i < this.shiftEvents.length; i++){
+      let shiftEvent = this.shiftEvents[i];
+      if(shiftEvent.isEqual(id)){
+        shiftEvent.setStatus(status);
+      }
+    }
+  }
 
+  // Cambiar el estado de un evento (por fecha)
+  private changeEventStatusByDate(date, status){
+    for(let i = 0; i < this.shiftEvents.length; i++){
+      let shiftEvent = this.shiftEvents[i];
+      let event = shiftEvent.getOriginalEvent();
+      if(date.toISOString() === event.start.toISOString()){
+        shiftEvent.setStatus(status);
+        break;
+      }
+    }
+    return
+  }
 
-
-
-
+  /* El calendario funciona un poco raro y cada vez que un usuario
+  edita una fecha, guarda automaticamente la hora del usuario por defecto.
+  De ahi este metodo que debe establecer a cero la fecha. Así se
+  evitan errores innecesarios.*/
+  private clearDateTime(d){
+    d.setSeconds(0);
+    d.setMilliseconds(0);
+    d.setHours(0);
+    d.setMinutes(0);
+  }
 
   /*Ventana de envio de petición de cambio*/
   private activateShift(){
@@ -579,6 +639,7 @@ export class AngularCalendarComponent implements AfterViewInit {
     this.showShiftAlert = false;
     // Se escoge el evento "libre" del usuario que se ha seleccionado para intercambio
     let selectedEvent:MyEvent = this.freeUsers[this.choosenFreeUser];
+
     // Se crea el intercambiao
     // El valor de la variable es ahora pendiente, por que falta confirmación
     let interchangeObject = <InterchangeDTO  > {
@@ -588,10 +649,13 @@ export class AngularCalendarComponent implements AfterViewInit {
       acknowledger_event_id : selectedEvent._id,
       status: "pending"
     };
+
     // Se cambia estado del evento por ID
     this.changeEventStatusById(this.eventForChosenDay._id, 'required');
+
     // Llamada http para intercambiar turnos
     this.calendarRepository.activateShift(interchangeObject).subscribe(result => {
+
     // Se refresca
       this.refresh.next();
     });
@@ -611,14 +675,10 @@ export class AngularCalendarComponent implements AfterViewInit {
     shiftEvent.setSender('');
     //El evento pasara a tener prioridad normal
     shiftEvent.setStatus('normal');
-
     // Llamada http para actualizar el evento
     this.calendarRepository.updateEvent(shiftEvent.getOriginalEvent()).subscribe(result => {
-
-
       // Actualiza el calendario por que ha cambiado el evento
       this.refresh.next();
-
       // Sincronizacion, todos los eventos deben tener prioridad normal
       let originalEvent = this.shiftEvents[index].getOriginalEvent();
       for(let j = 0; j < this.declineEvents.length; j++){
@@ -627,20 +687,27 @@ export class AngularCalendarComponent implements AfterViewInit {
           this.declineEventCounter--;
         }
       }
-
       for(let j = 0; j < this.acceptedEvents.length; j++){
         if(originalEvent._id == this.acceptedEvents[j]._id ){
           this.acceptedEvents[j].status = "normal";
           this.acceptedEventCounter--;
         }
       }
-
-
       this.flagContinue = false;
     });
   }
 
+  // Se fuerza a que los eventos sean ASINGADOS CONFORMES (cuando se produce el intercambio)
+  // En realidad esto lo he asumido yo, dado que si has intercambiado un turno
+  // se da por hecho que aceptas la operacion con el otro usuario y por tanto debes trabajar
+  makeAssignedShift(event){
+    event.type = this.types[0];
+    event.color.primary  = this.colors.blue.primary;
+    event.color.secondary = this.colors.blue.secondary;
+  }
 
-
- 
+  // No es posible hacer click cuando no hay usuarios libres
+  hideActivateShiftButton(){
+    return this.freeUsers.length == 0;
+  }
 }
