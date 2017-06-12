fbbfde919499ffefa080eb52a6b0e27b9eefead8
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-06-12 13:02:26 +0200

    reconstruct #server #routes

---
+++ BarterAPP-Browser/server/routes/event.router.js
@@ -0,0 +1,26 @@
+'use strict'
+//   Exportar el bjeto app para luego utilizar las rutas (en el fichero server.js)
+module.exports = function(app) {
+  var events = require('../controllers/event.controller');
+
+  // rutas para eventos
+  app.route('/events')
+    .get(events.list_all_events) //Obtener
+    .post(events.create_event); //Crear
+
+  app.route('/events/:eventId')
+    .put(events.update_event) //Actualizar
+    .delete(events.delete_event); //Eliminar
+
+  app.route('/company/:company/events')
+  .get(events.list_all_events_by_company); //Obtener
+
+  app.route('/company/:company/events/free/:searchDay/shift/:shift/except/:userName')
+  .get(events.list_all_free_events_by_company_by_day_by_shift_except_currentUser); //Obtener
+
+  app.route('/company/:company/:username/events')
+    .get(events.list_all_events_by_company_by_worker); //Obtener
+
+  app.route('/company/:company/:username/events/special/:status')
+     .get(events.list_all_special_events); //Obtener
+}
---
+++ BarterAPP-Browser/server/routes/interchange.router.js
@@ -0,0 +1,19 @@
+'use strict'
+//   Exportar el objeto app para luego utilizar las rutas (en el fichero server.js)
+module.exports = function(app) {
+  var interchange = require('../controllers/interchange.controller');
+
+  // rutas para eventos (intercambiar)
+  app.route('/interchange/accept')
+  	.post(interchange.accept_shift);
+
+  app.route('/interchange/activate')
+  	.post(interchange.activate_shift);
+
+  app.route('/interchange/:acknowledger/:idAcknowledgerEvent')
+ //   .get(interchange.get_interchange)
+  	.delete(interchange.decline);
+
+  app.route('/interchange/test')
+	.post(interchange.test_mongoskin);
+};
