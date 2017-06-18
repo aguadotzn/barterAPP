71554e0da78400b9229a3b708b562df8bd2dc03c
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-06-18 18:54:31 +0200

    heroku

--- BarterAPP-Browser/server/config/db.ts
+++ BarterAPP-Browser/server/config/db.ts
@@ -1,6 +1,5 @@
 const config = {
-  url:'aag:admin@ds125262.mlab.com:25262/barterappdatabase' //Online
-  //url : 'mongodb://localhost:27017/test' //Local
+  url : 'mongodb://localhost:27017/test' //Local
 };
 //Especificacion de la base de datos
 //Este el fichero que es necesario cambiar para pasar
