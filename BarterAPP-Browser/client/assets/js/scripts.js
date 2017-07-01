212ce0e6aefae06ab73117bf970d10a35c9825d8
Adrian Ag <aguadogarciaadrian@gmail.com>
2017-07-01 23:55:27 +0200

    adding css part

--- BarterAPP-Browser/client/assets/js/scripts.js
+++ BarterAPP-Browser/client/assets/js/scripts.js
@@ -1,23 +1,21 @@
 
-$(document).ready(function() {
-	
-    /*
+/*$(document).ready(function() {
+
+
         Fullscreen background
-    */
+
 	$.backstretch([
                    "assets/img/backgrounds/1.jpg"
                  , "assets/img/backgrounds/2.jpg"
                 ], {duration: 3000, fade: 750});
-    
-    /*
-        Form validation
-    */
+
+  
     $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
-    
+
     $('.login-form').on('submit', function(e) {
-    	
+
     	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
@@ -27,8 +25,8 @@ $(document).ready(function() {
     			$(this).removeClass('input-error');
     		}
     	});
-    	
+
     });
-    
-    
-});
+
+
+});*/
